4d4d371db16ce93dd4593f533c809d6586f313b7
Cornelis Heuperman <cheuperman@gmail.com>
2018-08-06 13:16:02 +0100

    Controller handles all

--- src/peep-controller.js
+++ src/peep-controller.js
@@ -1,11 +1,10 @@
 (function(exports) {
   function Controller() {
     this.peepsView = new PeepsView();
-    this.peepsModel = new PeepsModel();
   }
 
   Controller.prototype.showPeepList = function () {
-    this.peepsModel.fetchPeeps()
+    fetch('https://chitter-backend-api.herokuapp.com/peeps')
       .then(response => response.json())
       .then(peeps => this.peepsView.wrapInHTML(peeps))
       .then(wrappedPeeps => document.getElementById('peep-list').innerHTML = wrappedPeeps);
