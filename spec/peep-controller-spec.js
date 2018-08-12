6eeb60a594f35ad7127ab4920797b7ec7f68539f
Cornelis Heuperman <cheuperman@gmail.com>
2018-08-12 14:24:29 +0100

    [Testing] Isolate unit tests for controller

--- spec/peep-controller-spec.js
+++ spec/peep-controller-spec.js
@@ -1,18 +1,31 @@
 describe('PeepsController', function() {
   describe('.showPeeps', function () {
-    it('calls the fetchPeeps function of the model',() => {
-      var mockPromise = function() {};
-      var mockPeepsListModel = function() {};
-      mockPeepsListModel.fetchPeeps = function() {};
-      mockPromise.then = function() { return mockPromise; };
-      spyOn(mockPeepsListModel, 'fetchPeeps').and.returnValue(mockPromise);
-      var mockPeepsListView = jasmine.createSpy('mockPeepsListView');
-      var mockObject = { innerHTML: "" };
-      var mockDocument = { getElementById: function () { return mockElement; } };
-      var peepsController = new PeepsController(mockPeepsListModel, mockPeepsListView);
-      peepsController.showPeepsList(mockObject);
 
+    var mockPeepsListModel = function() {};
+    var mockPeepsListView = function() {};
+    var peepsController = new PeepsController(mockPeepsListModel, mockPeepsListView);
+    var mockPromise = function() {};
+    var mockObject = { innerHTML: "" };
+    mockPeepsListModel.fetchPeeps = function() { return mockPromise; };
+    mockPeepsListView.wrapInHTML = function() {};
+    mockPromise.then = function() { return mockPromise; };
+
+    it('calls the fetchPeeps function of the model',() => {
+      spyOn(mockPeepsListModel, 'fetchPeeps').and.returnValue(mockPromise);
+      peepsController.showPeepsList(mockObject);
       expect(mockPeepsListModel.fetchPeeps).toHaveBeenCalled();
+    });
+
+    it('calls the wrapinHTML function of the view with the result of the models fetch',() => {
+      var mockWrappedPromise = function() {};
+      mockWrappedPromise.then = function() {};
+      mockPromise.then = function(callback) {
+        callback('Peeps');
+        return mockWrappedPromise;
+       };
+      spyOn(mockPeepsListView, 'wrapInHTML');
+      peepsController.showPeepsList(mockObject);
+      expect(mockPeepsListView.wrapInHTML).toHaveBeenCalledWith('Peeps');
     });
   });
 });
